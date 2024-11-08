e30786651d2ac27c51b94c619bb1c23e8c097da7
hado0123 <hado_123@naver.com>
2024-11-08 10:34:00 +0900

    up

--- ch12/src/Quiz01_sol/todoSlice.js
+++ ch12/src/Quiz01_sol/todoSlice.js
@@ -5,9 +5,11 @@ const todoSlice = createSlice({
    initialState: [],
    reducers: {
       addTodo: (state, action) => {
+         // redux 툴킷은 불변성을 유지하므로 push 사용 가능
          state.push({ id: Date.now(), text: action.payload })
       },
       removeTodo: (state, action) => {
+         //filter, map 사용시 return을 하지 않으면 그냥 배열 그 자체만 만들어 지므로 반드시 리턴
          return state.filter((todo) => todo.id !== action.payload)
       },
    },
