55900026f977308d67c13033b669aadeab9047e0
hado0123 <hado_123@naver.com>
2024-11-09 16:38:38 +0900

    up

--- ch11/src/Quiz02_sol/counterReducer.js
+++ ch11/src/Quiz02_sol/counterReducer.js
@@ -4,8 +4,10 @@ function counterReducer(state = initialState, action) {
    switch (action.type) {
       case 'increment':
          return { count: state.count + 1 }
+      // return { ...state, count: state.count + 1 } 불변성 유지
       case 'decrement':
          return { count: state.count - 1 }
+      // return { ...state, count: state.count - 1 } 불변성 유지
       default:
          return state
    }
